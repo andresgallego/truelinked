a5d9fda746f8ab862628a046cf2ef0ccbdd40d5c
Andres Gallego <andres.gallego@sellics.com>
2020-09-13 12:50:43 +0200

    Test suite configuration

---
+++ src/pages/__tests__/posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Posts from '../posts';
+
+describe('Posts', () => {
+  // it('renders correctly', () => {
+  //   const tree = renderer
+  //     .create(<Posts location={{ pathname: '/posts/2' }} />)
+  //     .toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
+});
