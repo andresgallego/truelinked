a69423c5d407ca5d057fd4b42bac4ac6cf81351c
Andres Gallego <andres.gallego@sellics.com>
2020-09-14 16:41:41 +0200

    Make it look nice in small devices

--- src/components/post.js
+++ src/components/post.js
@@ -4,9 +4,11 @@ import styles from './post.module.css';
 
 export default function Post({ post }) {
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>{post.title}</h1>
-      <article>{post.body}</article>
-    </div>
+    <section className={styles.wrapper}>
+      <section className={styles.content}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <article>{post.body}</article>
+      </section>
+    </section>
   );
 }
